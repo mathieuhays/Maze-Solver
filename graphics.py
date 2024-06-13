78c7024747c01b4aa58ccabbfe7cfb3a1ef689c0
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-06-13 21:21:38 +0200

    add point and line and small refactor

--- graphics.py
+++ graphics.py
@@ -21,3 +21,21 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.a.x, self.a.y, self.b.x, self.b.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from window import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(10, 10), Point(500, 500)), "red")
+    win.draw_line(Line(Point(50, 90), Point(700, 200)), "black")
     win.wait_for_close()
 
 
