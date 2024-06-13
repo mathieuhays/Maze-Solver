55bb563f35d3a4652376514855c5f87dca740e93
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-06-14 00:39:51 +0200

    cell, make api consistent with boot.dev

--- main.py
+++ main.py
@@ -1,57 +1,37 @@
-from graphics import Window, Line, Point, Cell
+from graphics import Window, Line, Point
+from cell import Cell
 
 def main():
     win = Window(800, 600)
 
-    Cell(
-        win,
-        Point(150, 100),
-        Point(200, 150)
-    ).draw()
+    c = Cell(win)
+    c.draw(150, 100, 200, 150)
 
-    Cell(
-        win,
-        Point(250, 100),
-        Point(300, 150),
-        has_bottom_wall=False
-    ).draw()
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(250, 100, 300, 150)
 
-    Cell(
-        win,
-        Point(150, 200),
-        Point(200, 250),
-        has_right_wall=False
-    ).draw()
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(150, 200, 200, 250)
 
-    Cell(
-        win,
-        Point(250, 200),
-        Point(300, 250),
-        has_left_wall=False
-    ).draw()
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(250, 200, 300, 250)
 
-    Cell(
-        win,
-        Point(350, 200),
-        Point(400, 250),
-        has_top_wall=False
-    ).draw()
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(350, 200, 400, 250)
 
-    Cell(
-        win,
-        Point(200, 400),
-        Point(300, 500),
-        has_left_wall=False,
-        has_right_wall=False
-    ).draw()
+    c = Cell(win)
+    c.has_left_wall = False
+    c.has_right_wall = False
+    c.draw(200, 400, 300, 500)
 
-    Cell(
-        win,
-        Point(500, 400),
-        Point(600, 500),
-        has_top_wall=False,
-        has_bottom_wall=False
-    ).draw()
+    c = Cell(win)
+    c.has_top_wall = False
+    c.has_bottom_wall = False
+    c.draw(500, 400, 600, 500)
 
     win.wait_for_close()
 
